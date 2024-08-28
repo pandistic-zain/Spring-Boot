ead870b4cc46154092f8ebec29a028909e1b7390
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-08-28 06:32:48 -0700

    "Removed unnecessary imports in UserController.java"

--- journalapp/src/main/java/org/zain/journalapp/Controller/UserController.java
+++ journalapp/src/main/java/org/zain/journalapp/Controller/UserController.java
@@ -1,7 +1,5 @@
 package org.zain.journalapp.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +11,6 @@ import org.zain.journalapp.DAO.UserRepository;
 import org.zain.journalapp.Entity.UserEntity;
 import org.zain.journalapp.Services.UserServices;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
