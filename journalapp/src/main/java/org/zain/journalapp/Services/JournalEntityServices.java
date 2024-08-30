e9c2e8899735f7ccf31808ea97c90d5cb17fe270
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-08-30 05:52:14 -0700

    "Added transactional support to saveJournalEntity method, removed @Transactional annotation, and refactored code to use ClientSession for transaction management."

--- journalapp/src/main/java/org/zain/journalapp/Services/JournalEntityServices.java
+++ journalapp/src/main/java/org/zain/journalapp/Services/JournalEntityServices.java
@@ -6,49 +6,75 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.zain.journalapp.DAO.JournalEntityRepository;
 import org.zain.journalapp.Entity.JournalEntity;
 import org.zain.journalapp.Entity.UserEntity;
 
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+
 @Component
 public class JournalEntityServices {
+
     @Autowired
     private JournalEntityRepository journalEntityRepository;
+
     @Autowired
     private UserServices userServices;
-    @Transactional
-    public void saveJournalEntity(JournalEntity journalEntity , String username) throws Exception{
-       try {
-        UserEntity user = userServices.findByName(username);
 
-        journalEntity.setDate(LocalDateTime.now());
-        JournalEntity saved = journalEntityRepository.save(journalEntity);
-        user.getJournalEntities().add(saved);
-        userServices.saveNewUser(user);
-       } catch (Exception e) {
-        System.out.println("Exception Occured..."+ e);
-        throw new Exception("Error Occured ... ", e);
-       }
+     @Autowired
+    private MongoClient mongoClient;  
+
+    public void saveJournalEntity(JournalEntity journalEntity, String username) throws Exception {
+        ClientSession session = mongoClient.startSession();  // Use the injected MongoClient
+        
+        try {
+            session.startTransaction();
+
+            // Find user by username
+            UserEntity user = userServices.findByName(username);
+
+            // Save journal entry
+            journalEntity.setDate(LocalDateTime.now());
+            JournalEntity saved = journalEntityRepository.save(journalEntity);
+
+            // Add journal to user
+            user.getJournalEntities().add(saved);
+
+            // Save the updated user
+            userServices.saveNewUser(user);
+
+            // Commit transaction
+            session.commitTransaction();
+        } catch (Exception e) {
+            // Rollback transaction on error
+            session.abortTransaction();
+            System.out.println("Exception Occurred: " + e);
+            throw new Exception("Error Occurred", e);
+        } finally {
+            session.close();
+        }
     }
-    public void saveJournalEntity(JournalEntity journalEntity ) 
-    {
 
+
+    public void saveJournalEntity(JournalEntity journalEntity) {
         journalEntity.setDate(LocalDateTime.now());
         journalEntityRepository.save(journalEntity);
-
     }
-    
-    public List<JournalEntity> getAllEntities(){
+
+    public List<JournalEntity> getAllEntities() {
         return journalEntityRepository.findAll();
     }
-    public Optional<JournalEntity>findById(ObjectId id){
+
+    public Optional<JournalEntity> findById(ObjectId id) {
         return journalEntityRepository.findById(id);
     }
-    public boolean deleteJournalEntity(ObjectId id, String username) throws Exception{
+
+    public boolean deleteJournalEntity(ObjectId id, String username) throws Exception {
         UserEntity user = userServices.findByName(username);
-        user.getJournalEntities().removeIf(x-> x.getId().equals(id));
+        user.getJournalEntities().removeIf(x -> x.getId().equals(id));
         userServices.saveUser(user);
         journalEntityRepository.deleteById(id);
         return true;
